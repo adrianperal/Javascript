ee5734d6bd6bae33bf79ca158fde97e3994abfb7
Adrian Peral <adrian.peral@gmail.com>
2023-04-12 22:48:03 +0100

    wrap FizzBuzz

--- Problem-solver: FizzBuzz/assets/JS/index.js
+++ Problem-solver: FizzBuzz/assets/JS/index.js
@@ -1,10 +1,9 @@
 function fizzbuzz() {
-  let num = parseInt(prompt("Enter a number: "));
+  let num = parseInt(prompt("Enter a number form 1 to N"));
   let arr = [];
   while (true) {
-    if (isNaN(num)) {
-      alert("Please enter a number");
-      num = parseInt(prompt("Enter a number(no letters or words): "));
+    if (isNaN(num) || num < 1) {
+      num = parseInt(prompt("No letters or words, just positive natural numbers"));
     } else {
       for (let i = 1; i <= num; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
